2b1f7ce483f2dd8bb1e7d53fb1c36030d22cea3b
John Tarleton Moore <tarletonmoore@gmail.com>
2022-03-31 14:46:12 -0400

    commit tree ee4fb09c4b01cb5af13ba7f6e7118161aabd3a05

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Add your code here
+function submitData(name, email) {
+    const userObj = {
+        name: name,
+        email: email,
+    };
+
+
+    const configureObj = {
+        method: `POST`,
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(userObj),
+    };
+
+    return fetch("http://localhost:3000/users", configureObj)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            const body = document.querySelector(`body`)
+            body.textContent = object[`id`]
+            console.log(object);
+        })
+        .catch(function (error) {
+            const body = document.querySelector(`body`)
+            body.textContent = ""
+            alert("Unauthorized Access");
+            body.textContent = error[`message`]
+            console.log(error.message);
+        });
+}
+
+
+
+
+
+
+
+
+
+// function configureObj(submitData) {
+//     fetch(`http://localhost:3000/users`, {
+//         method: `POST`,
+//         headers: {
+//             "Content-Type": "application/json",
+
+//         },
+//         body: JSON.stringify(submitData)
+//     })
+//         .then(res => res.json())
+// };
+
